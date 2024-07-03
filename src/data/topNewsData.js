56872b458421d40c579aa2419fd4b9386a54eff0
sooyeoniya <tndus1306403@naver.com>
2024-07-03 18:34:28 +0900

    feat: 뉴스스탠드 데이터 추가 (in progress)

---
+++ src/data/topNewsData.js
@@ -0,0 +1,50 @@
+export const TOP_NEWS_DATA = {
+  data: [
+    {
+      "topNews": [
+        {
+          "id": 1,
+          "newsLists": [
+            {
+              "name": "연합뉴스",
+              "description": "채상병특검법 상정·필리버스터 돌입…대정부질문 또 파행",
+              "url": "https://www.yna.co.kr/view/AKR20240703119200001?section=politics/all&site=topnews01"
+            },
+            {
+              "name": "연합뉴스",
+              "description": "경찰 \"가해 운전자, 호텔 주차장 출입구 쪽부터 과속\"(종합)",
+              "url": "https://www.yna.co.kr/view/AKR20240703084951004?section=society/all&site=major_news01"
+            },
+            {
+              "name": "연합뉴스",
+              "description": "단식 들어간 서울아산병원 교수 \"환자·전공의들에게 미안해서\"",
+              "url": "https://www.yna.co.kr/view/AKR20240703142800530?section=society/all&site=major_news01"
+            },
+          ],
+          length: 3
+        },
+        {
+          "id": 2,
+          "newsLists": [
+            {
+              "name": "서울경제",
+              "description": "채상병특검법 상정·필리버스터 돌입…대정부질문 또 파행",
+              "url": "https://www.yna.co.kr/view/AKR20240703119200001?section=politics/all&site=topnews01"
+            },
+            {
+              "name": "서울경제",
+              "description": "경찰 \"가해 운전자, 호텔 주차장 출입구 쪽부터 과속\"(종합)",
+              "url": "https://www.yna.co.kr/view/AKR20240703084951004?section=society/all&site=major_news01"
+            },
+            {
+              "name": "서울경제",
+              "description": "단식 들어간 서울아산병원 교수 \"환자·전공의들에게 미안해서\"",
+              "url": "https://www.yna.co.kr/view/AKR20240703142800530?section=society/all&site=major_news01"
+            },
+          ],
+          length: 3
+        }
+      ],
+    }
+  ]
+}
