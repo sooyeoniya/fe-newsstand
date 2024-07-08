da5d354ec8127b9baa4dbcba1fd332768dd191f3
sooyeoniya <tndus1306403@naver.com>
2024-07-08 14:33:23 +0900

    feat: 전체 언론사 및 내가 구독한 언론사 버튼별 뉴스 스탠드 연결

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./utils/date/getDateString.js";
 import "./news/topNews/rolling.js";
+import "./news/topMenu/media.js";
 import "./news/newsStand/newsStand.js";
---
+++ src/news/topMenu/media.js
@@ -0,0 +1,23 @@
+
+function mediaButtonControl() {
+  const mediaTotalButton = document.querySelector('.media-total');
+  const mediaMyButton = document.querySelector('.media-my');
+  const mediaTotalView = document.querySelector('.media-total-view');
+  const mediaMyView = document.querySelector('.media-my-view');
+
+  mediaTotalButton.addEventListener('click', () => {
+    mediaTotalButton.classList.add('active');
+    mediaMyButton.classList.remove('active');
+    mediaTotalView.classList.add('active');
+    mediaMyView.classList.remove('active');
+  });
+
+  mediaMyButton.addEventListener('click', () => {
+    mediaTotalButton.classList.remove('active');
+    mediaMyButton.classList.add('active');
+    mediaTotalView.classList.remove('active');
+    mediaMyView.classList.add('active');
+  });
+}
+
+mediaButtonControl();
