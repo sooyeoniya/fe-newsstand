dd560b530f672f50c4cecb7022ea79eef995d9b2
sooyeoniya <tndus1306403@naver.com>
2024-07-12 10:19:03 +0900

    refactor: 뉴스 리스트 렌더링 공통 로직 분리

--- src/components/newsStand/newsList/SubscribedNewsList.js
+++ src/components/newsStand/newsList/SubscribedNewsList.js
@@ -1,6 +1,6 @@
 import { getTabsNews } from "../../../apis/NewsAPI.js";
-import { handleSubscribeButtonClick } from "../../subscription/SubscribeButtonControll.js";
-import { setTotalPages, setCurrentPage, getCurrentPage } from "../../state/StateManager.js";
+import { setTotalPages, getCurrentPage } from "../../state/StateManager.js";
+import { renderNewsItem } from "./NewsListRenderer.js";
 
 export default async function SubscribedNewsList() {
   const newsContainer = document.querySelector('.media-my-view .news-container');
@@ -30,42 +30,7 @@ export default async function SubscribedNewsList() {
     const currentPage = getCurrentPage() || 1;
     const newsItem = filteredNewsItems[currentPage - 1];
 
-    newsContainer.innerHTML = `
-      <div class="news-list">
-        <article class="news-item">
-          <div class="news-meta">
-            <img src="${newsItem.sourceLogo}" alt="${newsItem.mediaName} logo" class="news-source" />
-            <span class="news-date">${newsItem.newsDate} 편집</span>
-            <button class="subscribe-button" data-media-name="${newsItem.mediaName}">
-              ${subscriptionStatus[newsItem.mediaName] === 'Y' ? 'x' : '+ 구독하기'}
-            </button>
-          </div>
-          <div class="news-main-content">
-            <div class="news-thumbnail">
-              <img src="${newsItem.mainNews.thumbnailImage}" alt="뉴스 이미지" class="news-image" />
-              <div class="news-description">
-                <a href="${newsItem.mainNews.url}" target="_blank" rel="noopener noreferrer">${newsItem.mainNews.newsTitle}</a>
-              </div>
-            </div>
-            <div class="news-sub-content">
-              <ul class="news-headlines">
-                ${newsItem.subNews.map(subNews => `
-                  <li class="sub-news-headline">
-                    <a href="${subNews.url}" target="_blank" rel="noopener noreferrer">${subNews.newsTitle}</a>
-                  </li>
-                `).join('')}
-              </ul>
-              <div class="news-disclaimer">
-                ${newsItem.mediaName} 언론사에서 직접 편집한 뉴스입니다.
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-    `;
-
-    const subscribeButton = newsContainer.querySelector('.subscribe-button');
-    subscribeButton.addEventListener('click', handleSubscribeButtonClick);
+    renderNewsItem(newsContainer, newsItem, subscriptionStatus);
 
   } catch (error) {
     console.error(error);
