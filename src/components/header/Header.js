70f7b05c6a1064fa2869df256484eed9c13eaeb5
sooyeoniya <tndus1306403@naver.com>
2024-09-28 01:45:32 +0900

    refactor: Header, Headline 로직 분리

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,25 @@
 import { formatDate } from "../../utils/DateUtils.js";
 import "./Header.css";
 
-// 헤더 UI
-export default function Header(elementId) {
-  const headerElement = document.getElementById(elementId);
-
-  headerElement.innerHTML = `
+function createHeaderElement() {
+  return `
     <div class="header-logo">
       <img src="src/assets/favicon.svg" alt="뉴스스탠드" />
       <p>뉴스스탠드</p>
     </div>
     <div class="header-date"></div>
   `;
+}
 
+function updateHeaderDate(headerElement) {
   const headerDate = headerElement.querySelector(".header-date");
   headerDate.textContent = formatDate();
+}
 
+// 헤더 UI
+export default function Header(elementId) {
+  const headerElement = document.getElementById(elementId);
+  headerElement.innerHTML = createHeaderElement();
+  updateHeaderDate(headerElement);
   return headerElement;
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,18 +1,22 @@
 export const NEWS_API_URL = import.meta.env.VITE_NEWS_API_URL;
 
 export const API_ENDPOINTS = {
-    TOP_NEWS: "/topNewsData",
-    TAB_NEWS: "/tabNewsData"
+  TOP_NEWS: "/topNewsData",
+  TAB_NEWS: "/tabNewsData"
 }
 
 export const VIEW_TYPES = {
-    TOTAL: "total",
-    SUBSCRIBED: "subscribed"
+  TOTAL: "total",
+  SUBSCRIBED: "subscribed"
 }
 
 export const BUTTON_TYPES = {
-    LEFT: "left",
-    RIGHT: "right"
+  LEFT: "left",
+  RIGHT: "right"
 }
 
+export const HEADLINE_ITEMS = [ "headline-item-1" , "headline-item-2" ];
+
 export const ANIMATION_DURATION = 5000;
+
+export const ROLLING_INTERVAL = 1000;
