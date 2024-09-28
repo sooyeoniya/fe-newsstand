392cbe43d2d529d657a20ec31a885af7288d87c5
sooyeoniya <tndus1306403@naver.com>
2024-09-29 01:06:15 +0900

    refactor: SubscribedNewsList 함수 분리

--- src/components/newsStand/newsList/SubscribedNewsList.js
+++ src/components/newsStand/newsList/SubscribedNewsList.js
@@ -2,28 +2,38 @@ import { getTabsNews } from "../../../apis/NewsAPI.js";
 import { setSubTotalPages, getSubCurrentPage } from "../../state/StateManager.js";
 import { renderNewsItem } from "./NewsListRenderer.js";
 
+const NO_SUBSCRIPTIONS_MESSAGE = '<p class="no-subscribe">구독한 언론사가 없습니다.</p>';
+
+function getSubscribedMediaNames(subscriptionStatus) {
+  return Object.keys(subscriptionStatus).filter(
+    mediaName => subscriptionStatus[mediaName] === "Y"
+  );
+}
+
+async function getFilteredNewsItems(subscribedMediaNames) {
+  const newsTabs = await getTabsNews();
+  return newsTabs.flatMap(tab => 
+    tab.tabData.filter(newsItem => subscribedMediaNames.includes(newsItem.mediaName))
+  )
+}
+
 // 내가 구독한 언론사 리스트
 export default async function SubscribedNewsList() {
   const newsContainer = document.querySelector(".media-my-view .news-container");
   const subscriptionStatus = JSON.parse(localStorage.getItem("subscriptionStatus")) || {};
 
-  const subscribedMediaNames = Object.keys(subscriptionStatus).filter(
-    mediaName => subscriptionStatus[mediaName] === "Y"
-  );
+  const subscribedMediaNames = getSubscribedMediaNames(subscriptionStatus);
 
   if (subscribedMediaNames.length === 0) {
-    newsContainer.innerHTML = '<p class="no-subscribe">구독한 언론사가 없습니다.</p>';
+    newsContainer.innerHTML = NO_SUBSCRIPTIONS_MESSAGE;
     return;
   }
 
   try {
-    const newsTabs = await getTabsNews();
-    const filteredNewsItems = newsTabs.flatMap(tab =>
-      tab.tabData.filter(newsItem => subscribedMediaNames.includes(newsItem.mediaName))
-    );
+    const filteredNewsItems = await getFilteredNewsItems(subscribedMediaNames);
 
     if (filteredNewsItems.length === 0) {
-      newsContainer.innerHTML = '<p class="no-subscribe">구독한 언론사가 없습니다.</p>';
+      newsContainer.innerHTML = NO_SUBSCRIPTIONS_MESSAGE;
       return;
     }
 
