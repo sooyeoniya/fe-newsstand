0d0f999d45cd640d43e083c99a510c4d186cbb31
sooyeoniya <tndus1306403@naver.com>
2024-10-03 23:05:16 +0900

    refactor: SubscriptionManager 리팩토링

--- src/components/subscription/SubscriptionManager.js
+++ src/components/subscription/SubscriptionManager.js
@@ -2,16 +2,23 @@ import { getTabsNews } from "../../apis/NewsAPI.js";
 import { setSubscriptionStatus } from "../../helpers/subscriptionHelpers.js";
 import { SUBSCRIPTION_STATUS } from "../../constants/constants.js";
 
+function extractMediaName(newsTabs) {
+  return newsTabs.flatMap(tab => tab.tabData.map(newsItem => newsItem.mediaName));
+}
+
+function createInitSubscriptionStatus(mediaNames) {
+  return mediaNames.reduce((status, mediaName) => {
+    status[mediaName] = SUBSCRIPTION_STATUS.UNSUBSCRIBED;
+    return status;
+  }, {});
+}
+
 // 내가 구독한 언론사 데이터 초기화
 async function initSubscriptionStatus() {
-  const subscriptionStatus = {};
   try {
     const newsTabs = await getTabsNews();
-    newsTabs.forEach(tab => {
-      tab.tabData.forEach(newsItem => {
-        subscriptionStatus[newsItem.mediaName] = SUBSCRIPTION_STATUS.UNSUBSCRIBED;
-      });
-    });
+    const mediaNames = extractMediaName(newsTabs);
+    const subscriptionStatus = createInitSubscriptionStatus(mediaNames);
     setSubscriptionStatus(subscriptionStatus);
   } catch (error) {
     console.error(error);
