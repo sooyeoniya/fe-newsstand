e452c6a8e8c7c743875f867e907b9ccb24b96fdc
sooyeoniya <tndus1306403@naver.com>
2024-10-03 21:32:24 +0900

    refactor: NewsTabControll 리팩토링

--- src/components/newsStand/newsTab/NewsTabControll.js
+++ src/components/newsStand/newsTab/NewsTabControll.js
@@ -8,22 +8,40 @@ import { removeTabContent, updateTabContent } from "./NewsTabContents.js";
 import { animateProgressBar } from "./NewsTabAnimation.js";
 import { initNewsListRenderer } from "../newsList/NewsListManager.js";
 
+function activateTab(tab, newsTabs) {
+  tab.classList.add("active");
+  updateTabContent(tab, newsTabs);
+  animateProgressBar(tab, newsTabs);
+}
+
+function deactivateTab(tab) {
+  tab.classList.remove("active");
+  removeTabContent(tab);
+}
+
+function updateTabStatus(tab, index, activeTabIndex, newsTabs) {
+  if (index === activeTabIndex) activateTab(tab, newsTabs);
+  else deactivateTab(tab);
+}
+
 // 활성화 탭으로 변경(활성화 시 탭 내부 구조 변경 및 프로그래스바 진행 추가를 위함)
 function updateActiveTab(newsTabs) {
   const tabsContainer = getTabsContainer();
   const tabs = tabsContainer.querySelectorAll(".tab");
   const activeTabIndex = getTotalActiveTabIndex();
+
+  tabs.forEach((tab, index) => updateTabStatus(tab, index, activeTabIndex, newsTabs));
+  initNewsListRenderer();
+}
+
+function handleTabClick(clickedTab, tabs, newsTabs) {
   tabs.forEach((tab, index) => {
-    if (index === activeTabIndex) {
-      tab.classList.add("active");
-      updateTabContent(tab, newsTabs);
-      animateProgressBar(tab, newsTabs);
-    } else {
-      tab.classList.remove("active");
-      removeTabContent(tab);
+    if (tab === clickedTab) {
+      setTotalActiveTabIndex(index);
+      setTotalPageCount(1);
+      updateActiveTab(newsTabs);
     }
   });
-  initNewsListRenderer();
 }
 
 // 클릭한 카테고리 탭으로 이동
@@ -31,16 +49,10 @@ function addTabClickListener(newsTabs) {
   const tabsContainer = getTabsContainer();
   tabsContainer.addEventListener("click", (event) => {
     const clickedTab = event.target.closest(".tab");
-    if (!clickedTab) return;
-
-    const tabs = tabsContainer.querySelectorAll(".tab");
-    tabs.forEach((tab, index) => {
-      if (tab === clickedTab) {
-        setTotalActiveTabIndex(index);
-        setTotalPageCount(1);
-        updateActiveTab(newsTabs);
-      }
-    });
+    if (clickedTab) {
+      const tabs = tabsContainer.querySelectorAll(".tab");
+      handleTabClick(clickedTab, tabs, newsTabs);
+    }
   });
 }
 
