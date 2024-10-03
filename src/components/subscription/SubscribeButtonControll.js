529c75727f676e3fed82303421df387e42c06829
sooyeoniya <tndus1306403@naver.com>
2024-10-03 22:17:17 +0900

    refactor: SubscribeButtonControll 리팩토링

--- src/components/subscription/SubscribeButtonControll.js
+++ src/components/subscription/SubscribeButtonControll.js
@@ -4,26 +4,41 @@ import SubscribedNewsList from "../newsStand/newsList/SubscribedNewsList.js";
 import { getSubscriptionStatus, setSubscriptionStatus } from "../../helpers/subscriptionHelpers.js";
 import { SUBSCRIBE_BUTTON_TEXT, SUBSCRIPTION_STATUS } from "../../constants/constants.js";
 
+async function handleSubscription(subscriptionStatus, mediaName, subscribeButton) {
+  subscriptionStatus[mediaName] = SUBSCRIPTION_STATUS.SUBSCRIBED;
+  subscribeButton.textContent = SUBSCRIBE_BUTTON_TEXT.UNSUBSCRIBE;
+  ToastMessage("내가 구독한 언론사에 추가되었습니다.");
+  return true;
+}
+
+async function handleUnsubscription(subscriptionStatus, mediaName, subscribeButton) {
+  const confirmed = await Notification(mediaName);
+  if (confirmed) {
+    subscriptionStatus[mediaName] = SUBSCRIPTION_STATUS.UNSUBSCRIBED;
+    subscribeButton.textContent = SUBSCRIBE_BUTTON_TEXT.SUBSCRIBE;
+  }
+  return confirmed;
+}
+
 // 구독 처리 기능
 async function handleSubscribeButtonClick(event) {
   const subscribeButton = event.target;
   const mediaName = subscribeButton.dataset.mediaName;
   const subscriptionStatus = getSubscriptionStatus();
+  console.log(subscriptionStatus);
+  const isCurrentlySubscribed = subscriptionStatus[mediaName] === SUBSCRIPTION_STATUS.SUBSCRIBED;
 
-  if (subscriptionStatus[mediaName] === SUBSCRIPTION_STATUS.UNSUBSCRIBED) {
-    subscriptionStatus[mediaName] = SUBSCRIPTION_STATUS.SUBSCRIBED;
-    subscribeButton.textContent = SUBSCRIBE_BUTTON_TEXT.UNSUBSCRIBE;
-    ToastMessage("내가 구독한 언론사에 추가되었습니다.");
+  let actionPerformed = false;
+  if (isCurrentlySubscribed) {
+    actionPerformed = await handleUnsubscription(subscriptionStatus, mediaName, subscribeButton);
   } else {
-    const confirmed = await Notification(mediaName);
-    if (confirmed) {
-      subscriptionStatus[mediaName] = SUBSCRIPTION_STATUS.UNSUBSCRIBED;
-      subscribeButton.textContent = SUBSCRIBE_BUTTON_TEXT.SUBSCRIBE;
-    }
+    actionPerformed = await handleSubscription(subscriptionStatus, mediaName, subscribeButton);
   }
 
-  setSubscriptionStatus(subscriptionStatus);
-  await SubscribedNewsList();
+  if (actionPerformed) {
+    setSubscriptionStatus(subscriptionStatus);
+    await SubscribedNewsList();
+  }
 }
 
 export { handleSubscribeButtonClick };
